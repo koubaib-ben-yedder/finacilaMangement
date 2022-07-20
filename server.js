bc4f790ab39cb47d0aba4dcb9acd41817f588eaf
koubaib ben yedder <koubaibbneyedder@gmail.com>
2022-07-20 19:58:55 +0200

    make it better

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use("/income",authentication,incomeRouter)
 app.use("/client",authentication,clientRouter)
 app.use("/user",authentication,userRouter)
 app.use("",publicRouter)
-app.set(port, (process.env.PORT || 5000));
+app.set(process.env.port||8000, (process.env.PORT || 5000));
 app.use('/static', express.static(path.join(__dirname, 'images')))
 console.log(__dirname)
 
