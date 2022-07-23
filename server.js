197e8a7bc6b333bac99665168481346b326ea64c
koubaib ben yedder <koubaibbneyedder@gmail.com>
2022-07-23 18:23:38 +0200

    make it better

--- server.js
+++ server.js
@@ -28,20 +28,7 @@ app.set("port", port);
 app.use('/static', express.static(path.join(__dirname, 'images')))
 console.log(__dirname)
 
-// Curb Cores Error by adding a header here
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-    );
-    next();
-  });
-  
+
 
 if(process.env.NODE_ENV == 'production') {
 
