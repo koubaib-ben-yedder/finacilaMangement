20b53d894544f705f6791db48efa32b3d3dbe02f
koubaib ben yedder <koubaibbneyedder@gmail.com>
2022-07-23 11:07:50 +0200

    deploy app confih=guration

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.json())
 connect()
 setup()
 const port=process.env.port||8000
-app.use(cors())
+
 
 app.use("/edit",authentication,editRouter)
 app.use("/factor",authentication,factorRouter)
@@ -28,7 +28,20 @@ app.set("port", port);
 app.use('/static', express.static(path.join(__dirname, 'images')))
 console.log(__dirname)
 
-
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+    );
+    next();
+  });
+  
 
 if(process.env.NODE_ENV !== 'production') {
 
