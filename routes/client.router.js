98a2577018f2cf3bdd2245d22bfa114b24022801
koubaib ben yedder <koubaibbneyedder@gmail.com>
2022-08-18 01:11:48 +0300

    client router

--- routes/client.router.js
+++ routes/client.router.js
@@ -2,9 +2,9 @@ const {getClient,getOneClient,updateClient,deleteClient,addClient}=require("../c
 const express=require("express")
 const clientRouter=express.Router()
 const {validateClient,validationClient}=require("../middlewares/validator/client.validater")
-clientRouter.get("/getClient",getClient)
+clientRouter.get("/getClient/:pageNumber",getClient)
 clientRouter.get("/getOneClient/:id",getOneClient)
-clientRouter.post("/addClient",addClient)
+clientRouter.post("/addClient",validateClient,validationClient,addClient)
 clientRouter.delete("/deleteClient/:id",deleteClient)
 clientRouter.put("/updateClient/:id",validateClient,validationClient,updateClient)
 
