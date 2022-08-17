8f1acaa441697315db82a7688a47f28384430479
koubaib ben yedder <koubaibbneyedder@gmail.com>
2022-08-18 01:04:53 +0300

    user router

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,10 +1,12 @@
-const {getOneUser,deleteUser,addUser,updateUser,login}=require("../controllers/user.controller")
+const {getOneUser,deleteUser,addUser,updateUser,login,getUser}=require("../controllers/user.controller")
 const {validateUser,validationUser}=require("../middlewares/validator/user.validater")
 const {validateLogin,validationLogin}=require("../middlewares/validator/login.validator")
 const express=require("express")
 const userRouter=express.Router()
 
 userRouter.get("/getOneUser",getOneUser)
+userRouter.get("/getUser",getUser)
+
 userRouter.delete("/deleteUser",deleteUser)
 userRouter.put("/updateUser",validateUser,validationUser,updateUser)
 
