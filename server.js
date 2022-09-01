80c7d6a34345de0445ec12674594666a8284b3e2
koubaib ben yedder <koubaibbneyedder@gmail.com>
2022-09-02 11:16:57 +1400

    nothink to commit

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ setup()
 const port=process.env.PORT||8000
 
 app.use(cors())
+
 app.use("/edit",authentication,editRouter)
 app.use("/factor",authentication,factorRouter)
 app.use("/income",authentication,incomeRouter)
