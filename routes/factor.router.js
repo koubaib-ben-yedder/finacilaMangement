613558f78d802b7f15f982e45d0ad1fe9f393899
koubaib ben yedder <koubaibbneyedder@gmail.com>
2022-09-02 20:03:49 +1400

    nothink to commit

--- routes/factor.router.js
+++ routes/factor.router.js
@@ -5,6 +5,7 @@ const {validateFactor,validationFactor}=require("../middlewares/validator/factor
 const {upload} =require("../middlewares/upload/multer")
 const {cloudinaryFn}  =require("../middlewares/upload/cloudinary")
 factorRouter.get("/getFactor/:pageNumber",getFactor)
+
 factorRouter.get("/getOneFactor/:id",getOneFactor)
 factorRouter.post("/addFactor",upload.single("imageFactor"),validateFactor,validationFactor,cloudinaryFn,addFactor)
 factorRouter.put("/updateFactor/:id",upload.single("imageFactor"),validateFactor,validationFactor,cloudinaryFn,updateFactor)
