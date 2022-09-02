dc9259ec4415a9b4e35be685adc6b9c3ef4f4bdf
koubaib ben yedder <koubaibbneyedder@gmail.com>
2022-09-02 18:40:55 +1400

    nothink to commit

--- frontend/src/compoment/router/beforeLoginRouter.js
+++ frontend/src/compoment/router/beforeLoginRouter.js
@@ -1,35 +1,18 @@
+import { useRoutes } from "react-router-dom";
 
-import {useRoutes} from "react-router-dom"
+import SingUp from "../js/singUp";
+import Login from "../js/login";
+import Home from "../js/home";
 
-import SingUp from "../js/singUp"
-import Login from "../js/login"
-import Home from "../js/home"
+const BeforeLoginRouter = () => {
+  const Route = useRoutes([
+    { path: "/login", exact: true, element: <Login /> },
+    { path: "/singup", element: <SingUp /> },
+    { path: "/", exact: true, element: <Home /> },
+  ]);
+ 
 
-const BeforeLoginRouter=()=>{
+  return Route;
+};
 
-    const Route=useRoutes([
-        {path:"/login",exact:true,element:<Login/>},
-        {path:"/singup",element:<SingUp/>},
-       
-        {path:"/",exact:true,element:<Home />},
-
-    ])
-    /*
-    return(<div>
-        <Routes>
-            <Route path="/" exact="true" element={<Main/>}/>
-            <Route path="/edit" elemen t={<Edit/>}/>
-            <Route path="/factor" elemnt={<Factor/>}/>
-            <Route path="/icnome" element={<Income />}/>
-            <Route path="/user" element={<User/>}/>
-            <Route path="/who" element={<Who/>}/>
-        </Routes>
-    </div>)*/
-
-return Route
-
-    
-
-}
-
-export default BeforeLoginRouter
+export default BeforeLoginRouter;
